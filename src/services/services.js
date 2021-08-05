3ed8267f69dd12c22a2ad8d2faab74130e376c61
Petter Rein <petter.rein@dfind.no>
2021-08-05 18:42:36 +0200

    add try catch block, console log errors

--- src/services/services.js
+++ src/services/services.js
@@ -7,9 +7,16 @@ translate.key = process.env.REACT_APP_GOOGLE_KEY;
 
 const translateText = async (inputText, fromLan, toLan) => {
   if (toLan !== "no") {
-    const translatedText = await translate(inputText, { from: fromLan, to: toLan }).then(text => {
-      return text
-    });
+    let translatedText = ""
+
+    try {
+      translatedText = await translate(inputText, { from: fromLan, to: toLan }).then(text => {
+        return text
+      });
+    } catch (e) {
+      console.log(e)
+      return inputText
+    }
 
     return translatedText
   }
