3dcd8b12ba3a3fd3ea75073f8cf78eda7c66dbb7
Petter Rein <petter.rein@dfind.no>
2021-08-05 11:37:39 +0200

    add react router for routing of components

--- src/App.js
+++ src/App.js
@@ -2,14 +2,28 @@ import React from "react"
 import './App.css';
 import Header from "./components/header/Header";
 import Search from "./pages/search"
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+} from "react-router-dom";
 
 
 function App() {
   return (
-    <div className="App">
-        <Header />
-        <Search />
-    </div>
+    <Router>
+      <div className="App">
+          <Header />
+          <Switch>
+            <Route path="/vaccine-registration">
+              <div>Hi</div>
+            </Route>
+            <Route path="/">
+              <Search />
+            </Route>
+          </Switch>
+      </div>
+    </Router>
   );
 }
 
