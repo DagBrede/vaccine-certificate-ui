b132d11db77019445b5856416135a588c834e09a
FilipJF-Dfind <filip.felberg@dfind.no>
2021-08-05 10:50:00 +0200

    Change directory structure

---
+++ src/pages/search.js
@@ -0,0 +1,14 @@
+import React from "react"
+import './search.css';
+import CertificateContainer from "../components/certificate-search/CertificateContainer";
+import Header from "../components/header/Header";
+
+function Search() {
+  return (
+    <div className="App">
+        <CertificateContainer />
+    </div>
+  );
+}
+
+export default Search;
