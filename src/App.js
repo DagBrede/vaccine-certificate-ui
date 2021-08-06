e0ba4366b53746c71ec54bdf6d3fe530e9f3face
kape142 <kape142@hotmail.com>
2021-08-06 09:39:39 +0200

    added support for adding vaccinations

--- src/App.js
+++ src/App.js
@@ -1,13 +1,14 @@
 import React from "react"
 import './App.css';
 import CertificateContainer from "./components/certificate-search/CertificateContainer";
+import VaccinationForm from "./components/add-vaccination/VaccinationForm";
 import Header from "./components/header/Header";
 
 function App() {
   return (
     <div className="App">
         <Header />
-        <CertificateContainer />
+        <VaccinationForm />
     </div>
   );
 }
